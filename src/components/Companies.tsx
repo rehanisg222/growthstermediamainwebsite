27a072372d81d045c8819a45c54f346f2e5c60db
rehanisg222 <rehannotfoundyt@gmail.com>
2025-05-18 20:37:42 +0530

    Update Companies section with 3x2 grid layout

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -49,7 +49,7 @@ const Companies: React.FC = () => {
                 <img 
                   src={company.logo} 
                   alt={`${company.name} logo`}
-                  className="h-16 w-auto object-contain filter brightness-0 invert opacity-90 hover:opacity-100 transition-opacity duration-300"
+                  className="h-40 w-auto object-contain filter brightness-0 invert opacity-90 hover:opacity-100 transition-opacity duration-300"
                 />
               </div>
             ))}
